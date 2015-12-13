55a45f6d9030664317d4e6f97b9094fea07d16c9
Robbert <robbert@vdzon.com>
2015-12-13 19:38:40 +0100

    fix create remote browser

--- cucumber/src/test/java/com/vdzon/msw/cucumber/browsers/BrowserFactory.java
+++ cucumber/src/test/java/com/vdzon/msw/cucumber/browsers/BrowserFactory.java
@@ -74,6 +74,7 @@ public class BrowserFactory {
     }
 
     public static WebDriver createRemoteDriver() {
+        System.out.println("***** CREATE REMOTE DRIVER 1");
         DesiredCapabilities capability = DesiredCapabilities.firefox();
 //        DesiredCapabilities capability = DesiredCapabilities.internetExplorer();
         RemoteWebDriver driver = null;
@@ -84,7 +85,7 @@ public class BrowserFactory {
         System.out.println("Selanium server:"+seleniumurl);
 
         try {
-            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+            driver = new RemoteWebDriver(new URL(seleniumurl), capability);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
